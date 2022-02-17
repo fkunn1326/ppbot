409179057183ce4dc0ea4cbe14599c7a4fc0a33c
Fkunn1326 <92153597+fkunn1326@users.noreply.github.com>
2022-02-17 00:06:41 +0000

    定期コミット

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+import os
+from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
 from server import keep_alive
+import json
+
+Client = commands.Bot(command_prefix="!")
+slash = SlashCommand(Client, sync_commands=True)
+
+json_open = open('config.json', 'r')
+json_load = json.load(json_open)
+
+@slash.slash(name="test", description="test command")
+async def test(ctx):
+    await ctx.send("pong!")
 
 
+keep_alive()
+
+Client.run(os.environ['DISCORD_TOKEN'])
